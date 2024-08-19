dd6c1dc06a4938e3c8cda9340231327cb1fae025
Breno Weslley Santana Mattos <bwsantana68@gmail.com>
2024-08-19 03:58:34 -0300

    atualizei

---
+++ CRIANDO O SITE/pagina home/script.js
@@ -0,0 +1,8 @@
+function isubmenu (){
+    var menu = document.getElementById('submenu')
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none'
+    } else{
+        menu.style.display ='block'
+    }
+}
