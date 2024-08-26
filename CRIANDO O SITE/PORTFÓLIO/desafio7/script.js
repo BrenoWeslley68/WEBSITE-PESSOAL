d086bbdb6c1cb2a02c75e8b0b3704b74445d20af
Breno Weslley Santana Mattos <bwsantana68@gmail.com>
2024-08-26 01:48:17 -0300

    atualizei

--- CRIANDO O SITE/PORTFÓLIO/desafio7/script.js
+++ CRIANDO O SITE/PORTFÓLIO/desafio7/script.js
@@ -1,57 +1,55 @@
-let contador = 0
-let input = document.getElementById('itarefa');
-let add = document.getElementById('add');
-let section = document.getElementById('areaLista')
-function Adicionar(){
-    //pegar o valor digitado no input
-    if(input.value !== '' && input.value !== null && input.value !== 0){
+let contador = 0;
+let input = document.getElementById('inputTarefa');
+let btnAdd = document.getElementById('btn-add');
+let main = document.getElementById('areaLista');
 
-        contador++;
-        
-        
-        let novoItem = `<div id="${contador}" class="item clicado">
-                <div onclick = "marcarTarefa(${contador})" class="itemicone">
-                    <span class="material-symbols-outlined">
-                        radio_button_unchecked
-                        </span>
-                </div>
-                <div class="itemnome">
-                    ${input.value}
-                </div>
-                <div class="itembotao">
-                    <button id="delete" onclick = "Deletar(${contador})">
-                        <span class="material-symbols-outlined">
-                            disabled_by_default
-                            </span> deletar
-                    </button>
-                </div>`
-                //ADICIONAR NOVO ITEM NA SECTION
-                section.innerHTML += novoItem
-
-                //ZERAR OS CAMPOS
-                input.value = ''
-                input.focus()
-    } 
+function addTarefa() {
+    // Pegar o valor digitado no input
+    let valorInput = input.value;
+    // Se não for vazio, nulo ou indefinido
+    if (valorInput !== '' && valorInput !== null && valorInput !== undefined) {
+        ++contador;
+        let novoItem = `<div id="${contador}" class="item">
+            <div onclick="marcarTarefa(${contador})" class="item-icone">
+                <input type="checkbox" id="itemclicado${contador}">
+            </div>
+            <div onclick="marcarTarefa(${contador})" class="item-nome">
+                ${valorInput}
+            </div>
+            <div class="item-botao">
+                <button onclick="Deletar(${contador})" class="delete">
+                    <span class="material-symbols-outlined">delete</span>Deletar
+                </button>
+            </div>
+        </div>`;
+        // Adicionar novo item no main
+        main.innerHTML += novoItem;
+        // Zerar os campos
+        input.value = '';
+        input.focus();
     }
-    function Deletar(id){
-        let tarefa = document.getElementById(id);
-        tarefa.remove()
+}
 
+function Deletar(id) {
+    let tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id) {
+    let item = document.getElementById(id);
+    let checkbox = document.getElementById('itemclicado' + id);
+
+    if (checkbox.checked) {
+        item.classList.add('clicado');
+    } else {
+        item.classList.remove('clicado');
     }
-    function marcarTarefa(id){
-        let item = document.getElementById(id)
-        let classe = item.getAttribute('class');
-        console.log(classe)
-        if(classe == 'item'){
-            item.classList.add('clicado')
-        }
+}
 
+input.addEventListener('keyup', function(event) {
+    // Se teclou Enter (13)
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        btnAdd.click();
     }
-
-input.addEventListener('keyup', function (event) {
-    //SE TECLOU ENTER ADICIONA
-    if(event.keyCode === 13){
-        event.preventDefault()
-        add.click()}
-        
-})
+});
